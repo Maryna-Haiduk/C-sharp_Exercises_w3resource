a066271c4d41bf09d608e79423807ee5435b420f
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-06-19 13:22:16 -0400

    Ordered

---
+++ Exercise_12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    /* 
+     * 12. Write a C# program to that takes a number as input 
+     * and display it four times in a row (separated by blank spaces), 
+     * and then four times in the next row, with no separation. 
+     * You should do it two times: Use Console. Write and then use {0}.
+     */
+    internal class Program
+    {        
+        public static int InputNumber()
+        {
+            Console.Write("Enter a digit: ");
+            int.TryParse(Console.ReadLine(), out int inputNum);
+            return inputNum;
+        }
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                int x = InputNumber();
+                Console.WriteLine("\n{0} {0} {0} {0}\n{0}{0}{0}{0}\n{0} {0} {0} {0}\n{0}{0}{0}{0}", x);
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Exercise_7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    /* 
+     * 7. Write a C# Sharp program to print on screen the output of 
+     * adding
+     * subtracting
+     * multiplying
+     * dividing 
+     * of two numbers which will be entered by the user
+     */
+    internal class Program
+    {
+        public static void Calculate(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} = {a / b}");
+            Console.WriteLine($"{a} % {b} = {a % b}");
+
+        }
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Calculate(num1, num2);
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
